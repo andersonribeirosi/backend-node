9693812ad55a136a13185c596c11c9e7bd979675
Anderson <andersonribeiro.si@hotmail.com>
2023-10-28 03:43:41 -0300

    .

--- build/src/middleware/authentications.d.ts
+++
@@ -1,2 +0,0 @@
-import { Request } from 'express';
-export declare const AuthenticationMiddleware: (req: Request<{}, any, any, any, Record<string, any>>, res: any, next: any) => Promise<void>;
--- build/src/mongoose/dataSource/counters.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class CountersDataSource {
-    models: any;
-    constructor(models: any);
-    createSeqNext<T>(collectionName: string): Promise<any>;
-    seqNext(collectionName: string): Promise<any>;
-}
